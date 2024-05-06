c2e59fc109a91d79d9a51db035c027f4f6aa890a
fayinana <fekedeananiya@gmail.com>
2024-05-06 17:28:16 +0300

    all sections are end but not with responsive lay out

---
+++ assets/js/data.js
@@ -0,0 +1,20 @@
+Shoes = [
+  {
+    name: "Aliquam lobortis",
+    type: 1,
+    color: "#b3e141",
+    imgPath: "./assets/images/banner.png",
+  },
+  {
+    name: "FUSCE ALIQUAM",
+    type: 2,
+    color: "rgb(246, 107, 107)",
+    imgPath: "./assets/images/gray.png",
+  },
+  {
+    name: "AIR SUPERIORITY",
+    type: 3,
+    color: "rgb(77, 77, 77)",
+    imgPath: "./assets/images/whiteblack.png",
+  },
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,12 @@ const navBtn = document.querySelector(".nav-menu");
 const navMenuLists = document.querySelector(".nav-lists");
 const navMenuList = document.querySelectorAll(".nav-list");
 
+const selectedImg = document.querySelector(".select-images");
+const yearVertical = document.querySelector(".vertival-year");
+const date = new Date();
+yearVertical.innerHTML = date.getFullYear();
+innerHtml = "";
+htmlGenerator();
 navBtn.addEventListener("click", () => {
   navBtn.style.display = "none";
   navMenuLists.style.display = "flex";
@@ -20,3 +26,40 @@ navMenuList.forEach((navList) => {
     navList.classList.add("active");
   });
 });
+
+function htmlGenerator() {
+  Shoes.forEach((shoe) => {
+    innerHtml += `
+	  <div class="select-image" data-type = "${shoe.type}">
+		<div class="inner">
+			<img src=${shoe.imgPath} alt="">
+		</div>
+	  </div>`;
+  });
+  selectedImg.innerHTML = innerHtml;
+  changeImage(selectedImg);
+}
+
+function changeImage(selectedImg) {
+  const images = selectedImg.querySelectorAll(".select-image");
+  addBcColor(images[0]);
+  images.forEach((image) => {
+    image.addEventListener("click", () => {
+      images.forEach((img) => {
+        img.style.background = "white";
+      });
+      let type = image.dataset.type;
+      addBcColor(image, type);
+    });
+  });
+}
+
+function addBcColor(image, type = "1") {
+  Shoes.forEach((shoe) => {
+    if (type == shoe.type) {
+      bannerImg = document.querySelector(".banner-image img");
+      image.style.background = shoe.color;
+      bannerImg.src = `${shoe.imgPath}`;
+    }
+  });
+}
